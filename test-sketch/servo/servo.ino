840145d1f07203e41e49cee8d8205c8c1266047a
joseferncruz <29801616+joseferncruz@users.noreply.github.com>
2020-06-14 12:57:09 -0400

    new training sketches

--- test-sketch/servo/servo.ino
+++ test-sketch/servo/servo.ino
@@ -1,9 +1,35 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.println("Servo Ready");
+  delay(5000);
+  Serial.println("Reset servo position");
+  myservo.write(0);
+  delay(3000);
+  Serial.println("Servo is at initial position");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("Initial position: 0");
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(3000);
+  Serial.println("Final position: 90");
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  delay(5000);
+  
 }
