55ae8baf37fd9e519d3898f4aaf13e712f5a2913
joseferncruz <29801616+joseferncruz@users.noreply.github.com>
2020-08-20 09:52:10 -0400

    add push button

--- arduino_protocols/platform_avoidance/MAGAZINE_TRAINING_CBN/MAGAZINE_TRAINING_CBN.ino
+++ arduino_protocols/platform_avoidance/MAGAZINE_TRAINING_CBN/MAGAZINE_TRAINING_CBN.ino
@@ -7,7 +7,7 @@ Jose Oliveira da Cruz, jose.cruz@nyu.edu
 
 MAGAZINE_TRAINING_COUL
 ----------------------
-This protocols is build to habituate a subject to the food dispenser in the Coulbourn Chambers. 
+Deliver one food pellet per minute. 
 
 
  */
@@ -16,10 +16,11 @@ This protocols is build to habituate a subject to the food dispenser in the Coul
 #include <Stepper.h>
 
 
-const int stepsPerRevolution = 200;  // steps per revolution
-const int food_tray_led = 8;        // LED in the chamber food tray 
+const int stepsPerRevolution = 200;     // steps per revolution
+const int food_tray_led = 8;            // LED in the chamber food tray 
+const int push_button = 2;              // push button to start the protocol
 const int acclimation_len = 3*60;       // Len in seconds. Same as cooldown
-const int magazine_trials = 10;      // Number of times the magazine will be activated 
+const int magazine_trials = 10;         // Number of times the magazine will be activated 
 
 
 // initialize the stepper library
@@ -30,40 +31,45 @@ void setup() {
   // initialize the serial port:
   Serial.begin(9600);
   pinMode(food_tray_led, OUTPUT);
+  pinMode(push_button, INPUT);
   Serial.println("LEDOUX LAB");
-  Serial.println("MAGAZINE_TRAINING");
-  Serial.println("STARTING IN (sec): 10");
-  delay(10 * 1000L);
+  Serial.println("MAGAZINE_TRAINING: ONE PELLET/MIN FOR TEN MINUTES");
+  Serial.println("PRESS GREEN BUTTON TO START");
   
 }
 
 void loop() {
-  Serial.println("SESSION: START");
-  delay(1000);
   
-  // ACCLIMATION
-  Serial.print("ACCLIMATION (MIN): ");
-  Serial.println(acclimation_len);
-  delay(acclimation_len * 1000L);
-
-  // DELIVER FOOD PELLET EACH ONE MINUTE FOR n MINUTES
-  for (int x = 1; x < magazine_trials; x++) {
-  Serial.println("MAGAZINE: ON");
-  myStepper.step(stepsPerRevolution/2);            // EQUIVALENT TO 1/2 PELLETS
-  digitalWrite(food_tray_led, HIGH);
-  delay(3*1000L);
-  digitalWrite(food_tray_led, LOW);
-  delay(57*1000L);
-  Serial.print("REMAINING MINUTES: ");
-  Serial.println(magazine_trials - x);
+  int button_state = 0;
+  button_state = digitalRead(push_button);
+  
+  if (button_state == HIGH) {
+    
+    Serial.println("SESSION: START");
+    delay(1000);
+    
+    // ACCLIMATION
+    Serial.print("ACCLIMATION (SEC): ");
+    Serial.println(acclimation_len);
+    delay(acclimation_len * 1000L);
+  
+    // DELIVER ONE FOOD PELLET EACH ONE MINUTE FOR N MINUTES
+    for (int x = 1; x < magazine_trials; x++) {
+    Serial.println("MAGAZINE: ON");
+    myStepper.step(stepsPerRevolution/2);            // EQUIVALENT TO 1/2 PELLETS
+    digitalWrite(food_tray_led, HIGH);
+    Serial.print("REMAINING MINUTES: ");
+    Serial.println(magazine_trials - x);
+    delay(3*1000L);
+    digitalWrite(food_tray_led, LOW);
+    delay(57*1000L);
+    }
+  
+    // COOLDOWN
+    Serial.print("COOLDOWN (SEC): ");
+    Serial.println(acclimation_len);
+    delay(acclimation_len * 1000L);
+    Serial.println("SESSION: END");
+    
   }
-
-  // COOLDOWN
-  Serial.print("COOLDOWN (MIN): ");
-  Serial.println(acclimation_len);
-  delay(acclimation_len * 1000L);
-  Serial.println("SESSION: END");
-  
-  // ENDLESS LOOP TO ALLOW EXPERIMENTER TO SWITCH SUBJECT AND RESTART
-  while(1) { }
 }
