d5831cac9170520c17d94b0af39c94ba5f5eb7a7
joseferncruz <29801616+joseferncruz@users.noreply.github.com>
2020-08-21 16:44:16 -0400

    refactor void setup

--- arduino_protocols/platform_avoidance/PLAT_AVOID_THREAT_PROTOCOL_CBN_ARD02/PLAT_AVOID_THREAT_PROTOCOL_CBN_ARD02.ino
+++ arduino_protocols/platform_avoidance/PLAT_AVOID_THREAT_PROTOCOL_CBN_ARD02/PLAT_AVOID_THREAT_PROTOCOL_CBN_ARD02.ino
@@ -6,59 +6,73 @@
  * jose.cruz@nyu.edu
  * 
  * 
- * 
- * 
+ * PLATFORM AVOIDANCE THREAT PROTOCOL - CODE FOR ARDUINO02 - ARD02
  * 
  * 
  * 
  * --------------------------------------------------------------------------------
  */
 
-// Constants
+// SETTINGS OF CS/US/SESSIONS
 
-const int acclimation_seconds = 5 * 60L; // IN SECONDS
-const int cooldown_seconds = 5 * 60L;    // IN SECONDS
-
-const int cs_len = 30;              // DURATION CS
+const int cs_len = 30;             // DURATION CS
 const int us_len = 2;              // DURATION US
+int total_cs_number = 5;           // NUMBER OF CS 
 
-// Variables
+int itintervals[] = {60, 90, 120, 160, 180}; // list of the inter-trial-intervals: ITI
 
-int total_cs_number = 5;          // NUMBER OF CS 
+// ACCLIMATION AND COOLDOWN
+
+const int acclimation_seconds = 3 * 60L; // IN SECONDS
+const int cooldown_seconds = 3 * 60L;    // IN SECONDS
+
+
+// OTHERS
 
 int switchstate = 0; // Button starts the experiment
 int switchstate_test_led = 0; // Test the led
 int detect_cs = 0; // detect if the cs was delivered.
 int detect_us = 0; // detect if the us was delivered
 
-// list of the inter-trial-intervals: ITI
-int itintervals[] = {60, 90, 120, 160, 180};
-
 
 void setup() {
   // Start serial
-  Serial.begin(9600); // serial will be read by bonsai
-  delay(5 * 1000);
-  Serial.println("LEDOUX LAB");
+  Serial.begin(9600); 
   
-  // Digital Output pins
-  pinMode(7, OUTPUT); // Delivering the CS
-  pinMode(8, OUTPUT); // Delivering the US
-  pinMode(9, OUTPUT); // Delivering the CHAMBER LED when the CS is delivered.
-  pinMode(11, OUTPUT); // Signal the deliver of CS to LED
-  pinMode(12, OUTPUT); // Signal the deliver of US to LED
-  pinMode(13, OUTPUT); // control arduino01
+  // PRINT INITIAL INFORMATION
+  Serial.println("----------------------------------");
+  Serial.println("LEDOUX LAB");
+  Serial.println("PLATFORM AVOIDANCE");
+  Serial.println("----------------------------------");
+  Serial.print("CS LEN (SEC): "); Serial.println(cs_len);
+  Serial.print("US LEN (SEC): "); Serial.println(us_len);
+  Serial.print("US STARTS AT "); Serial.print(cs_len - us_len); Serial.println("(SEC) AFTER CS ONSET");
+  Serial.print("NUMBER OF CS-US PAIRS: "); Serial.println(total_cs_number);
+  Serial.print("ACCLIMATION (SEC): "); Serial.print(acclimation_seconds); 
+  Serial.print(" | COOLDOWN (SEC): "); Serial.println(cooldown_seconds);
+  Serial.println("----------------------------------");
+  Serial.println("PRESS BLUE BUTTON TO START");
+  Serial.println("----------------------------------");
+  
+  // DIGITAL OUTPUT
+  pinMode(7, OUTPUT);              // Delivering the CS
+  pinMode(8, OUTPUT);              // Delivering the US
+  pinMode(9, OUTPUT);              // Delivering the CHAMBER LED when the CS is delivered.
+  pinMode(11, OUTPUT);             // Signal the deliver of CS to LED
+  pinMode(12, OUTPUT);             // Signal the deliver of US to LED
+  pinMode(13, OUTPUT);             // control arduino01
   
 
-  // Button to start the experiment.
-  pinMode(2, INPUT); // single puch to start
-  pinMode(3, INPUT); // button to test LEDs
+  // DIGITAL INPUT
+  pinMode(2, INPUT);               // PUSH BUTTON TO START
+  pinMode(3, INPUT);               // TEST LEDS
   
 }
 
 void loop() {
 
-  // TURN CHAMBER LED ON
+
+  // TEST CHAMBER LED
   switchstate_test_led = digitalRead(3);
 
   if (switchstate_test_led == HIGH) {
@@ -73,60 +87,34 @@ void loop() {
     Serial.println("CHAMBER LED: OFF");
   }
   
+  
   //int total_cs_number = 5; // How many CS is going to be delivered.
   
-  // variable to keep track of the number of CS at a give time
-  int current_cs = 1;
+  int current_cs = 1; // KEEP TRACK OF THE CURRENT CS
 
 
   // START NEW EXPERIMENT
   switchstate = digitalRead(2);
   if (switchstate == HIGH) { 
 
-    Serial.println("NEW EXPERIMENT: CLASSICAL THREAT CONDITIONING");
-    delay(1000);
-    Serial.println("SESSION: CONDITIONING");
-    delay(1000);
-    
-    // TEST CHAMBER LEDs
-    Serial.println("TEST CHAMBER LED");
-    Serial.println("LED: ON");
-    digitalWrite(9, HIGH);
-    delay(10*1000); // 10 seconds
-    digitalWrite(9, LOW);
-    Serial.println("LED: OFF");
-    delay(5 * 1000);
-
-    //  PRINT INFORMATION ABOUT SESSION
-
-    Serial.print("NUMBER OF CSs: ");
-    Serial.println(total_cs_number);
-    delay(500);
-
-    Serial.print("CS DURATION (sec): ");
-    Serial.println(cs_len);
-    delay(500);
-    
-    Serial.print("NUMBER OF USs: ");
-    Serial.println(total_cs_number);
-    delay(500);
-
-    Serial.print("US DURATION (sec): ");
-    Serial.println(us_len);
-    delay(500);
-
-    Serial.print("US START AT (CS DURATION - US DURATION): ");
-    Serial.println((cs_len - us_len));
-    delay(500);
 
     // START THE EXPERIMENT
     
-    Serial.println("NEW EXPERIMENT: START");
-    delay(1000);
+    Serial.println("SESSION: START");
 
-    Serial.print("ACCLIMATION (sec): ");
-    Serial.println(acclimation_seconds);
+    // SIGNAL ARDUINO 01 TO START OWN CODE
+    Serial.println("START LEVER PRESS PROTOCOL ON ARDUINO 01");
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+
+    // ACCLIMATION
+    Serial.print("ACCLIMATION (SEC): "); Serial.println(acclimation_seconds);
+    // WAIT UNTIL THE END OF THE ACCLIMATION
     delay(acclimation_seconds*1000L);
+
+    
+    // THREAT PROTOCOL 
     
     while (total_cs_number > 0) {
 
