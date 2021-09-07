de1acfb9304ae752f6bc5f2801ebe8972b3cec63
Audrey Yin <ay2376@nyu.edu>
2021-09-07 10:36:21 -0400

    bug: Revert previous commit
    Previous commit logic is incorrect. Using a nested if statement causes
    the yellow_state variable to be held as HIGH or LOW for a longer period
    of time. The previous commit blinking looked bad.

--- arduino_protocols/m_active_avoidance/m_active_avoidance.ino
+++ arduino_protocols/m_active_avoidance/m_active_avoidance.ino
@@ -198,11 +198,13 @@ void setup() {
     unsigned long currentMillis = millis();
 
     if (currentMillis - previousMillis >= interval) {
-      yellow_state = HIGH;
+      // put yellow_state = HIGH
       previousMillis = currentMillis;
-      digitalWrite(check_yellow_LED, yellow_state);
-    } else {
-      yellow_state = LOW;
+      if (yellow_state == LOW){
+        yellow_state = HIGH;
+      } else{
+        yellow_state = LOW;
+      }
       digitalWrite(check_yellow_LED, yellow_state);
     }
   }
