604a75851d69f56bcd1d059806f4ea621c597281
Audrey Yin <ay2376@nyu.edu>
2021-09-13 10:45:05 -0400

    style: Reorganize variables subsection

--- arduino_protocols/r_active_avoidance/r_active_avoidance.ino
+++ arduino_protocols/r_active_avoidance/r_active_avoidance.ino
@@ -12,30 +12,46 @@
 #include <SharpIR.h>
 
 
-// VARIABLES
-//#########################################################
+/*
+    VARIABLES
+*/
+//##################################################################################################################
+// EXPERIMENTAL VARIABLES
 const int N_TRIALS = 20;
 unsigned long ACCLIMATION_DURATION = 20;                       // SECONDS
 unsigned long TONE_DURATION = 15;                              // SECONDS
 unsigned long SHOCK_DURATION = 1;                              // SECONDS
-int CS_FREQUENCY = 5000;                                  // IN HERTZ
+int CS_FREQUENCY = 5000;                                       // IN HERTZ
 int ITI_INTERVALS[] = {40, 60, 80, 100, 120};                  // list of the inter-trial-intervals: ITI
-
-// LOCATION VARIABLES.
-// ########################################################
+unsigned long MOTION_DETECTION_DURATION = 30;                  // SECONDS
+//##################################################################################################################
+// LOCATION VARIABLES
 int LEFT_ACTIVE;                                        // HIGH IF A COMPARTMENT IS ACTIVE, ELSE LOW
 int RIGHT_ACTIVE;
-
-// ######################################
+//##################################################################################################################
+// TIMING VARIABLES
+// For control of US and CS
 unsigned long CURRENT_TONE_DELAY;
 unsigned long START_TONE;
 unsigned long DELTA_TONE_SHOCK = TONE_DURATION - SHOCK_DURATION;
 unsigned long ITI_DURATION;
 
-// timing variables for motion detection
+// For control of motion detection
 unsigned long MOTION_DETECTION_START;
 unsigned long MOTION_DETECTION_CURR;
-unsigned long MOTION_DETECTION_DURATION = 30;               // SECONDS
+//##################################################################################################################
+// VARIABLES FOR STATISTICS
+unsigned long ESCAPE_LATENCY_START;
+unsigned long ESCAPE_LATENCY_END;
+unsigned long ESCAPE_LATENCY_DELTA;
+float ESCAPE_LATENCY_CUMULATIVE;
+//##################################################################################################################
+// SESSION VARIABLES
+int TOTAL_AVOIDANCE_SUCCESS = 0;                           // CUMULATIVE COUNT OF SUCCESSFUL AVOIDANCE RESPONSES
+int TOTAL_AVOIDANCE_FAILURE = 0;                           // CUMULATIVE COUNT OF FAILED AVOIDANCE RESPONSES
+//int ESCAPE_LATENCY_INDIVIDUAL[N_TRIALS];                   // LIST WITH THE LATENCY. 0 == NO SHUTTLE (== FAILURE)
+//##################################################################################################################
+
 
 /*
     PIN ASSIGNMENTS
@@ -88,20 +104,8 @@ SharpIR IR_SENSOR_L2 = SharpIR(ir_left2, model);
 
 // SENSOR THRESHOLDS
 int IR_THRESHOLD = 20;                                   // CM > DISTANCE FROM SENSOR TO OPPOSITE WALL.
+//##################################################################################################################
 
-
-
-// VARIABLES FOR STATISTICS
-// ##########################################################
-unsigned long ESCAPE_LATENCY_START;
-unsigned long ESCAPE_LATENCY_END;
-unsigned long ESCAPE_LATENCY_DELTA;
-float ESCAPE_LATENCY_CUMULATIVE;
-
-// SESSION
-int TOTAL_AVOIDANCE_SUCCESS = 0;                           // CUMULATIVE COUNT OF SUCCESSFUL AVOIDANCE RESPONSES
-int TOTAL_AVOIDANCE_FAILURE = 0;                           // CUMULATIVE COUNT OF FAILED AVOIDANCE RESPONSES
-//int ESCAPE_LATENCY_INDIVIDUAL[N_TRIALS];                   // LIST WITH THE LATENCY. 0 == NO SHUTTLE (== FAILURE)
 
 
 void setup() {
