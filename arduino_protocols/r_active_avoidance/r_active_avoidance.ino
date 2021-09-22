1b719a402e8228999fc00aab4d86ec040c4206b6
Audrey Yin <ay2376@nyu.edu>
2021-09-22 11:40:16 -0400

    style: Move all pin assignments together

--- arduino_protocols/r_active_avoidance/r_active_avoidance.ino
+++ arduino_protocols/r_active_avoidance/r_active_avoidance.ino
@@ -63,28 +63,44 @@ int TOTAL_AVOIDANCE_SUCCESS = 0;                           // CUMULATIVE COUNT O
 int TOTAL_AVOIDANCE_FAILURE = 0;                           // CUMULATIVE COUNT OF FAILED AVOIDANCE RESPONSES
 
 
+/*
+    PIN ASSIGNMENTS
+*/
+//##################################################################################################################
 // DIGITAL PINS
-// ########################################################
+// Speakers
 const int speaker_pin = 3;
 
+// Shockers
 const int shocker_r_pin = 4;
 const int shocker_l_pin = 5;
 
+// Buzzers
 const int buzzer_pin_r = 6;
 const int buzzer_pin_l = 7;
+
+// IR LED lights
+const int speaker_led_r = 9;
+const int speaker_led_l = 10;
 
 // LED Check Lights (for UX design)
 const int check_red_LED = 23;
 const int check_yellow_LED = 25;
 const int check_green_LED = 27;
+//##################################################################################################################
+// ANALOG PINS
+// Right Sensors
+#define ir_right1 A0
+#define ir_left1 A1
+
+// Left Sensors
+#define ir_right2 A2
+#define ir_left2 A3
+//##################################################################################################################
 
 Tone SPEAKER_RIGHT;
 Tone SPEAKER_LEFT;
 
-#define ir_right1 A0
-#define ir_left1 A1
-#define ir_right2 A2
-#define ir_left2 A3
 #define model 1080
 SharpIR IR_SENSOR_R1 = SharpIR(ir_right1, model);
 SharpIR IR_SENSOR_L1 = SharpIR(ir_left1, model);
@@ -92,8 +108,6 @@ SharpIR IR_SENSOR_R2 = SharpIR(ir_right2, model);
 SharpIR IR_SENSOR_L2 = SharpIR(ir_left2, model);
 int IF_THRESHOLD = 20;                                   // CM > DISTANCE FROM SENSOR TO OPPOSITE WALL.
 
-const int speaker_led_r = 9;
-const int speaker_led_l = 10;
 
 const int start_switch_pin = 22;
 const int test_switch_pin = 23;
